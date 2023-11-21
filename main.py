46adbb97934c306c0f31ef5f41e60329e879c3a6
DanielBugelnig <daniel.bugelnig@gmail.com>
2023-11-21 10:25:24 +0100

    Initial commit

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ def simple_connect():
     print("Yeah, I'm connected! :D")
     time.sleep(3)
     print("Now I will disconnect :'(")
-
+kkk
 if __name__ == '__main__':
     # Initialize the low-level drivers
     cflib.crtp.init_drivers()
